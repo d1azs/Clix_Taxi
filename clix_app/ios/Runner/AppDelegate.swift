301d7b4ce61a3cd921c4f3b43c9f3a3573fc5555
Tohqa <tohqa@MacBook-Air-Tohqa.local>
2026-05-25 13:21:26 +0300

    feat: fix iOS cocoapods configuration and AppDelegate GoogleMaps import to enable offline build

--- clix_app/ios/Runner/AppDelegate.swift
+++ clix_app/ios/Runner/AppDelegate.swift
@@ -1,6 +1,5 @@
 import Flutter
 import UIKit
-import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,10 +7,6 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    // Читаємо Google Maps API ключ із .env (через Flutter assets)
-    if let key = Bundle.main.infoDictionary?["GoogleMapsAPIKey"] as? String, !key.isEmpty {
-      GMSServices.provideAPIKey(key)
-    }
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
